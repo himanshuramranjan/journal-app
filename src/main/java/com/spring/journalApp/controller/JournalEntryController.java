ca182676a31cea5b710dcd3a50415ae3ec63bf00
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2024-02-06 06:20:02 +0530

    Basic structure for REST service added

---
+++ src/main/java/com/spring/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,51 @@
+package com.spring.journalApp.controller;
+
+import com.spring.journalApp.entity.JournalEntry;
+import com.spring.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/journal")
+public class JournalEntryController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return journalEntryService.getAll();
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry journalEntry) {
+        journalEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(journalEntry);
+        return journalEntry;
+    }
+    @PutMapping("id/{myId}")
+    public JournalEntry updateEntry(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
+        JournalEntry oldEntry = journalEntryService.findById(myId).orElse(null);
+        if(oldEntry != null) {
+            oldEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : oldEntry.getTitle());
+            oldEntry.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : oldEntry.getContent());
+        }
+        journalEntryService.saveEntry(oldEntry);
+        return oldEntry;
+    }
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId) {
+        return journalEntryService.findById(myId).orElse(null);
+    }
+    @DeleteMapping("id/{myId}")
+    public boolean deleteJournalById(@PathVariable ObjectId myId) {
+        journalEntryService.deleteById(myId);
+        return true;
+    }
+
+}
