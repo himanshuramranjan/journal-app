253cebccaf84125287e87016707bacd3c0e71373
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2024-02-07 20:57:15 +0530

    Transactional annotations added

--- src/main/java/com/spring/journalApp/service/JournalEntryService.java
+++ src/main/java/com/spring/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.spring.journalApp.repository.JournalEntryRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,6 +21,7 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
 
         try {
